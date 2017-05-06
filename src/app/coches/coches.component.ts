1fe833901e0981db862f8c5bb5fbac7693ff91c4
Unknown <alva85@gmail.com>
2017-05-05 22:51:23 -0500

    plantillas, servicios

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
+import { PeticionesService } from '../services/peticiones.Service';
 
 @Component({    
     selector: 'coches',
-    templateUrl: 'coches.component.html'
+    templateUrl: 'coches.component.html',
+    providers: [PeticionesService]
 })
 
 export class CochesComponent {
 
     public coche:Coche;    
     public coches:Array<Coche>;
+    public articulos;
 
-    constructor() { 
+    constructor(
+        private _peticionesService:PeticionesService
+    ) { 
         this.coche = new Coche("", "", "");
         this.coches = [
             new Coche("Coche 1", "100", "rojo"),
@@ -20,6 +25,20 @@ export class CochesComponent {
     }
 
     ngOnInit() { 
+        this._peticionesService.getArticulos().subscribe(
+            result => {
+                this.articulos = result;
+                console.log(result);
+
+                if(!this.articulos){
+                    console.log("Error en el servidor");
+                }
+
+            }, error => {
+                var err = <any>error;
+                console.log(err);
+            }
+        );
 
     }
 
